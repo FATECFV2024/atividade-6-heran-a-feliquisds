d0a655cf4dd6a8b48bb7cb4acb20442bb85b5890
feliquisds <mail@feliquisds.gay>
2024-11-27 11:57:37 -0300

    refactor(FolhaPGTO); use new print method

--- src/FolhaPGTO.java
+++ src/FolhaPGTO.java
@@ -7,32 +7,18 @@ public class FolhaPGTO {
 
         // mensalistas
         men1 = new Mensalista("Jose", "Rua abc", "Junior");
-        men1.calcularSalario();
-        men1.calcularINSS();
-        System.out.println("Nome: " + men1.getNome());
-        System.out.println("Endereço: " + men1.getEndereco());
-        System.out.println("Salário: " + men1.getSalario());
+        men1.imprimirInfo();
 
         men2 = new Mensalista("Ana", "Rua sem fim", "Senior");
-        men2.calcularSalario();
-        men2.calcularINSS();
-        System.out.println("Nome: " + men2.getNome());
-        System.out.println("Endereço: " + men2.getEndereco());
-        System.out.println("Salário: " + men2.getSalario());
+        men2.imprimirInfo();
 
 
 
         // horistas
         hora1 = new Horista("Carlos", "Rua xyz", 20);
-        hora1.calcularSalario();
-        System.out.println("Nome: " + hora1.getNome());
-        System.out.println("Endereço: " + hora1.getEndereco());
-        System.out.println("Salário: " + hora1.getSalario());
+        hora1.imprimirInfo();
 
         hora2 = new Horista("Cristina", "Rua do centro", 100);
-        hora2.calcularSalario();
-        System.out.println("Nome: " + hora2.getNome());
-        System.out.println("Endereço: " + hora2.getEndereco());
-        System.out.println("Salário: " + hora2.getSalario());
+        hora2.imprimirInfo();
     }
 }
