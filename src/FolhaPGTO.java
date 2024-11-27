a2a74493f37bddd39dfdaa98e9de84857ea6bf89
feliquisds <mail@feliquisds.gay>
2024-11-27 12:00:52 -0300

    fix(FolhaPGTO); re-add calcularSalario

--- src/FolhaPGTO.java
+++ src/FolhaPGTO.java
@@ -7,18 +7,22 @@ public class FolhaPGTO {
 
         // mensalistas
         men1 = new Mensalista("Jose", "Rua abc", "Junior");
+        men1.calcularSalario();
         men1.imprimirInfo();
 
         men2 = new Mensalista("Ana", "Rua sem fim", "Senior");
+        men2.calcularSalario();
         men2.imprimirInfo();
 
 
 
         // horistas
         hora1 = new Horista("Carlos", "Rua xyz", 20);
+        hora1.calcularSalario();
         hora1.imprimirInfo();
 
         hora2 = new Horista("Cristina", "Rua do centro", 100);
+        hora2.calcularSalario();
         hora2.imprimirInfo();
     }
 }
